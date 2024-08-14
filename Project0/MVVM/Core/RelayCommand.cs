879037eb17568c6f2721d808166218d56af83ebc
Apar Shrestha <shresthaapar.co@gmail.com>
2024-08-14 13:05:02 +0545

    Update Day1

---
+++ Project0/MVVM/Core/RelayCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ChatClient.MVVM.Core
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
---
+++ Project0/MVVM/Net/Server.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace ChatClient.MVVM.Net
+{
+    class Server
+    {
+        TcpClient _client;
+
+        public Server() 
+        {
+            _client = new TcpClient();
+        }
+
+        public void ConnectToServer()
+        {
+            if (!_client.Connected)
+            {
+                _client.Connect("127.0.0.1", 7891);
+            }
+        }
+
+    }
+}
